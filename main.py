97df1bb7c50d1b42ceed6a2ccea97df79aad3956
gaalbesa <galbesa@alumnat.copernic.cat>
2023-10-04 15:55:13 +0200

    Print the book information on screen scrapped from site.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from uuid import uuid1
 from bs4 import BeautifulSoup
 
 base_url = 'http://books.toscrape.com/'
@@ -6,5 +7,18 @@ page = requests.get(base_url + 'index.html')
 
 soup = BeautifulSoup(page.content, "html.parser")
 articles = soup.find_all("article", class_="product_pod")
-print(articles)
+
+for article in articles:
+    id = uuid1()
+    title_container = article.find('h3')
+    title = title_container.find('a').attrs['title']
+    
+    rating_container = article.find('p', class_="star-rating")
+    rating = rating_container.attrs['class'][1]
+    
+    price_container = article.find('p', class_="price_color")
+    price = price_container.text.replace('£','').strip()
+    
+    image_container = article.find('img', class_="thumbnail")
+    image = base_url + image_container.attrs['src']
     
