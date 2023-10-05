5204d30a95f877c2647112d9a527a232496e131f
gaalbesa <galbesa@alumnat.copernic.cat>
2023-10-05 12:23:25 +0200

    added text from API and handle the unauthorized exception.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import time
 from uuid import uuid1
 from bs4 import BeautifulSoup
 
@@ -9,6 +10,8 @@ soup = BeautifulSoup(page.content, "html.parser")
 articles = soup.find_all("article", class_="product_pod")
 
 books = dict()
+
+start = time.time()
 
 for article in articles:
     id = uuid1()
@@ -24,13 +27,32 @@ for article in articles:
     image_container = article.find('img', class_="thumbnail")
     image = base_url + image_container.attrs['src']
     
+    r = requests.post(
+    "https://api.deepai.org/api/text-generator",
+    data={
+        'text': f"The text of {title} is: ",
+    },
+    headers={'api-key': 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K'}
+    )
+    
+    if r.status_code == 401:
+        text = "Error obtaining the text from the API, the api-key is not valid or the ratio has been exceeded"
+    else:
+        text = r.json()['output']
+        
     books[id] = {
         'id': id,
         'title': title,
         'rating' : rating,
         'price' : price,
-        'image' : image   
+        'image' : image,
+        'text' : text 
     }
     
-    print(books)
+    print(books[id])
+
+    end = time.time()
+    print(f"Time taken since start {end-start} seconds")
+print(books)
+    
     
